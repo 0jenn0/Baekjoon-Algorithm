32603cfeda3ae4431c584ec7cebdefea9fcf2c2c
Jenn <130737187+0jenn0@users.noreply.github.com>
2024-01-02 22:11:35 +0900

    [Gold V] Title: 노드사이의 거리, Time: 348 ms, Memory: 12916 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.js
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.js
@@ -1,29 +1,30 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let [n, m] = input[0].split(' ').map(Number);
+let graph = Array.from({ length: n + 1 }, () => []);
 
-let [n,m] = input[0].split(' ').map(Number);
-let graph = [];
-for (let i = 1; i <= n; i++) graph[i] = [];
 for (let i = 1; i < n; i++) {
-    let [x,y,cost] = input[i].split(' ').map(Number);  
-    graph[x].push([y,cost]);
-    graph[y].push([x,cost]);
+    let [x, y, cost] = input[i].split(' ').map(Number);
+    graph[x].push([y, cost]);
+    graph[y].push([x, cost]);
 }
 
-function dfs(x,dist) {
-    if(visited[x]) return;
+let visited = new Array(n + 1); // 전역 스코프에서 선언
+let distance = new Array(n + 1);
+
+function dfs(x, dist) {
+    if (visited[x]) return;
     visited[x] = true;
     distance[x] = dist;
-    for(let [y,cost] of graph[x]) {
-        dfs(y,dist+cost);
+    for (let [y, cost] of graph[x]) {
+        dfs(y, dist + cost);
     }
 }
 
 for (let i = 0; i < m; i++) {
-    let [x,y] = input[n+i].split(' ').map(Number);
-    visited = new Array(n+1).fill(false); 
-    distance = new Array(n+1).fill(-1); 
-    dfs(x,0);
+    let [x, y] = input[n + i].split(' ').map(Number);
+    visited.fill(false); // 각 쿼리마다 배열을 초기화
+    distance.fill(-1);
+    dfs(x, 0);
     console.log(distance[y]);
 }
-
